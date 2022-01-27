77678c5da1835cc13b5747e89a409fed4dd66836
knedio <knedio.info@gmail.com>
2022-01-27 23:55:36 +0800

    updated nuxt config file

--- nuxt.config.js
+++ nuxt.config.js
@@ -50,11 +50,12 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-		baseURL: env.API_URL,
 		debug: true,
   },
 
-	env: env.parsed,
+	env: {
+    API_URL: env.parsed.API_URL
+  },
 
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
